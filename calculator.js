6072e420774bf9672d326edff12308cdeb198ef4
JayGulvi <140692864+JayGulvi@users.noreply.github.com>
2023-07-27 12:54:43 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let result = document.querySelector("#display")
+function calculate(number){
+    result.value += number
+}
+
+function clr(){
+    result.value = ""
+}
+function del(){
+    result.value = result.value.slice(0,-1)
+}
+function total(){
+    try {
+      result.value = eval(result.value )
+    } 
+    catch{
+        alert("expression error....")
+    }
+}
